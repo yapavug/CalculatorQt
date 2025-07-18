357822c4af2b27b35dad3c03c087a15a29f63e85
yapavug <pavelguliaev2018@mail.ru>
2025-07-18 14:22:18 +0300

    папка build/ была удалена

--- build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calculator_FILE D:/QtApp/Calculator/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/Calculator.exe)
-set(__QT_DEPLOY_TARGET_Calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calculator_RUNTIME_DLLS D:/msys64/mingw64/bin/Qt6Widgets.dll;D:/msys64/mingw64/bin/Qt6Gui.dll;D:/msys64/mingw64/bin/Qt6Core.dll)
