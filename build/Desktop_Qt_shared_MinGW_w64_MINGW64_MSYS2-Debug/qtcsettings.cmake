e5be9241cc446f802c5bdcf9d796a1af5efcb116
yapavug <pavelguliaev2018@mail.ru>
2025-07-18 14:19:14 +0300

    добавил build/ в gitignore

--- build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/qtcsettings.cmake
+++ build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/qtcsettings.cmake
@@ -1,10 +1,10 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/QtApp/Calculator/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "D:/msys64/mingw64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "D:/msys64/mingw64" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "D:/msys64/mingw64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "D:/msys64/mingw64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
 set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
 set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/msys64/mingw64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/msys64/mingw64" CACHE "PATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/QtApp/Calculator/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/msys64/mingw64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/msys64/mingw64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
